7536cbc776b74e5269d9b86cdd432d579dd5c962
Mikhail Zhukov <misha_zhukov94@bk.ru>
2022-08-12 16:43:45 +0300

    123

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -3,7 +3,7 @@ import FilterCheckbox from "../FilterCheckbox/FilterCheckbox";
 export default function SearchForm() {
   return (
     <form className="search-form">
-      <input className="search-form__entry" placeholder="Фильм" />
+      <input className="search-form__entry" placeholder="Фильм" required type="text"/>
       <button className="search-form__button" type="submit"></button>
       <FilterCheckbox />
     </form>
